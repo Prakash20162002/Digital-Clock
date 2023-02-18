c0fc45d42804dafaa607c13efeb91ba417f5c0d0
Prakash <Prakash.com>
2023-02-19 01:07:03 +0530

    nothing change

--- clock1.js
+++ clock1.js
@@ -2,13 +2,21 @@ const hours = document.getElementById("h");
 const Minutes = document.getElementById("m");
 const second = document.getElementById("s");
 const session = document.getElementById("ses");
+// const dateandtime = document.querySelector("dateandtime");
 function Showtime() {
     var date = new Date();
+    // var date = date.getDate()
+    // var year = date.getFullYear();
+    // var months = date.getMonth();
+    // var day = date.getDay();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
     var ses = "AM";
 
+
+    // var daysname = ["Saunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday", "Sunday"];
+    // var monthsname = ["January", "February", "March", "April", "May", "June", "July", "August", "Septembar", "Octobar", "Novembar", "December"];
 
     if (h == 0) {
         h = 12;
@@ -23,11 +31,13 @@ function Showtime() {
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
+    // date = (date < 10) ? "0" + date : date;
 
     hours.innerHTML = h;
     Minutes.innerHTML = m;
     second.innerHTML = s;
     session.innerHTML = ses;
+    // dateandtime.innerHTML = daysname[day] + "," + monthsname[months] + date + "," + year;
 
 
 
